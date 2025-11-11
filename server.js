9badd8eda19168ef63fb72ae3068eabb1a441911
Wei Hsiang Liao <flying.liao@gmail.com>
2025-11-11 14:57:01 +0800

    update server.js

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ try {
       res.json(result);
     });
 
+    app.get("/ping", (req, res) => {
+      res.status(200).send("pong");
+    });
+
+
     app.listen(process.env.PORT, () =>
       console.log(`🚀 Server running on port ${process.env.PORT}`)
     );
